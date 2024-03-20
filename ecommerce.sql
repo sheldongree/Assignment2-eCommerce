90d7edb99ea077286879fc97fd1daa41177815b1
Hussainamin10 <hussain.amin@icloud.com>
2024-03-20 01:52:26 -0400

    Updated database

--- ecommerce.sql
+++ ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 14, 2024 at 05:47 AM
+-- Generation Time: Mar 20, 2024 at 06:51 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -40,7 +40,8 @@ CREATE TABLE `profile` (
 --
 
 INSERT INTO `profile` (`profile_id`, `user_id`, `first_name`, `last_name`, `middle_name`) VALUES
-(3, 1, 'Mister', 'Blue', '');
+(22, 37, 'Hussain', 'Amin', 'K'),
+(24, 38, 'Sheldon', 'Green', 'J');
 
 -- --------------------------------------------------------
 
@@ -51,11 +52,19 @@ INSERT INTO `profile` (`profile_id`, `user_id`, `first_name`, `last_name`, `midd
 CREATE TABLE `publication` (
   `publication_id` int(11) NOT NULL,
   `profile_id` int(11) NOT NULL,
-  `publication_title` varchar(50) NOT NULL,
-  `publication_text` varchar(50) NOT NULL,
+  `publication_title` varchar(255) NOT NULL,
+  `publication_text` text NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
-  `publication_status` tinyint(4) NOT NULL DEFAULT 1
+  `publication_status` enum('public','private') NOT NULL DEFAULT 'public'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `publication`
+--
+
+INSERT INTO `publication` (`publication_id`, `profile_id`, `publication_title`, `publication_text`, `timestamp`, `publication_status`) VALUES
+(61, 22, 'Weather-Montreal', 'I think the weather in Montreal has been very nice lately!!', '2024-03-20 05:33:06', 'public'),
+(62, 24, 'Vanier', 'I think Vanier College\'s computer science department has been very crowed these days. Anyone knows why??', '2024-03-20 05:35:07', 'public');
 
 -- --------------------------------------------------------
 
@@ -67,8 +76,16 @@ CREATE TABLE `publication_comment` (
   `publication_comment_id` int(11) NOT NULL,
   `publication_id` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
-  `profile_id` int(11) NOT NULL
+  `profile_id` int(11) NOT NULL,
+  `comment_text` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `publication_comment`
+--
+
+INSERT INTO `publication_comment` (`publication_comment_id`, `publication_id`, `timestamp`, `profile_id`, `comment_text`) VALUES
+(15, 61, '2024-03-20 06:35:44', 24, 'Yeah, you\'re right. I feel that summer is backk my friend!!!!!');
 
 -- --------------------------------------------------------
 
@@ -87,7 +104,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `username`, `password_hash`) VALUES
-(1, 'Tarzan', '$2y$10$.c/0uj/ezTONgPUYzD/8auuOtxC.pllo8LOotHZVecYKP3XV3EmgK');
+(37, 'Hussain', '$2y$10$cPtSUMd6GV6wwqcZxPHQ4.PRCHqFMa3S5OY.nFyvEWonyjM9kChqC'),
+(38, 'Sheldon', '$2y$10$bZMBGUrCYaih//A5jVjN9OgjxkkJpXeOGZWdoDE74tlOhm0rGE2BS');
 
 --
 -- Indexes for dumped tables
@@ -130,13 +148,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `profile`
 --
 ALTER TABLE `profile`
-  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `publication`
+--
+ALTER TABLE `publication`
+  MODIFY `publication_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+
+--
+-- AUTO_INCREMENT for table `publication_comment`
+--
+ALTER TABLE `publication_comment`
+  MODIFY `publication_comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- Constraints for dumped tables
@@ -147,6 +177,19 @@ ALTER TABLE `user`
 --
 ALTER TABLE `profile`
   ADD CONSTRAINT `profile_to_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `publication`
+--
+ALTER TABLE `publication`
+  ADD CONSTRAINT `publication_to_profile` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`profile_id`);
+
+--
+-- Constraints for table `publication_comment`
+--
+ALTER TABLE `publication_comment`
+  ADD CONSTRAINT `publication_comment_ibfk_1` FOREIGN KEY (`publication_id`) REFERENCES `publication` (`publication_id`),
+  ADD CONSTRAINT `publication_comment_to_profile` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`profile_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
